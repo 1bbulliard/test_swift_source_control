33955bf19d780f94246e2036520a083f34c4e5b0
robert bulliard <bbulliard@gmail.com>
2021-10-13 14:59:59 -0500

    Initial Commit

---
+++ test version control/test_version_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_version_controlApp.swift
+//  test version control
+//
+//  Created by Robert Bulliard on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct test_version_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
