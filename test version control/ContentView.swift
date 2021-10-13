def93a714262009a0c098f80e972fe38833f8091
robert bulliard <bbulliard@gmail.com>
2021-10-13 15:46:27 -0500

    updated comments again

--- test version control/ContentView.swift
+++ test version control/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         Text("Hello, world!")
             .padding()
             .padding()
-        // test comments
+        // test comments  xx
         //test comments
     }
 }
